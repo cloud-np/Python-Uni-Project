acb7a11efaeaf1b701701c00bb2b53bed35161fb
dbouros <jimmisp92@gmail.com>
2021-05-07 19:32:35 +0300

    Filling the list version 1.0

--- project_parser.py
+++ project_parser.py
@@ -10,38 +10,68 @@ path_to_scl = r"C:\Users\lenovo\Desktop\Python_Project\Python-Uni-Project\data\d
 
 def get_node_stats():
 
+    # Make list here with size of len(nodes)
+    nodes_list = []
     with open(path_to_nodes, 'r') as nodes_file:
         nodes = nodes_file.readlines()[7:]
+        nodes_counter = 0
+
         for line in nodes:
             if re.match(re.compile(r"\s*[a|p]{1}[0-9]{1,2}\s*"), line):
+
+                nodes_counter += 1
                 i = 0
                 while i < len(line):
 
                     # Getting the names of the nodes
                     if re.match(re.compile(r"[a|p]"), line[i-2]) and re.match(re.compile(r"[0-9]"), line[i-1]) and re.match(re.compile(r"[0-9]|\s"), line[i]):
                         if re.match(re.compile(r"[0-9]"), line[i]):
-                            print(line[i-2] + line[i-1] + line[i])
+                            #print(line[i-2] + line[i-1] + line[i])
+                            name = line[i-2] + line[i-1] + line[i]
+                            #print(name)
+
                         elif re.match(re.compile(r"\s"), line[i]):
-                            print(line[i-2] + line[i-1])
+                            #print(line[i-2] + line[i-1])
+                            name = line[i-2] + line[i-1]
+                            #print(name)
 
                         # Getting the width of the nodes
                         if re.match(re.compile(r"\s"), line[i+11]):
-                            print(line[i+12])
+                            #print(line[i+12])
+                            width = line[i+12]
+                            #print(width)
                         elif re.match(re.compile(r"\s"), line[i+12]):
                             if re.match(re.compile(r"\s"), line[i+10]):
-                                print(line[i + 11])
+                                #print(line[i + 11])
+                                width = line[i + 11]
+                                #print(width)
                             else:
-                                print(line[i + 10] + line[i+11])
+                                #print(line[i + 10] + line[i+11])
+                                width = line[i + 10] + line[i+11]
+                                #print(width)
                         else:
-                            print(line[i+11] + line[i+12])
+                            #print(line[i+11] + line[i+12])
+                            width = line[i+11] + line[i+12]
+                            #print(width)
 
                         # Getting the height of the nodes
                         if re.match(re.compile(r"\s"), line[i + 23]):
                             if re.match(re.compile(r"\s"), line[i + 21]):
-                                print(line[i+22])
+                                #print(line[i+22])
+                                height = line[i+22]
+                                #print(height)
                             else:
-                                print(line[i+21] + line[i+22])
+                                #print(line[i+21] + line[i+22])
+                                height = line[i+21] + line[i+22]
+                                #print(height)
                         else:
-                            print(line[i+22] + line[i+23])
+                            #print(line[i+22] + line[i+23])
+                            height = line[i+22] + line[i+23]
+                            #print(height)
                     i += 1
 
+                # append to list here
+                nodes_list.append( Node(nodes_counter, name, width, height) )
+
+        # Test prints of the nodes_list down here:
+        print(len(nodes_list))
