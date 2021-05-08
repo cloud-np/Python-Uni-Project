8e3dcc823a59257129006eebe09521042a55cc13
CloudAnarchy <cloudanarchy@gmail.com>
2021-05-08 17:12:13 +0300

    Node Parser done

--- classes/node.py
+++ classes/node.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, id_, name, width, height):
+    def __init__(self, id_: int, name: str, width: int, height: int):
         self.name = name
         self.width = width
         self.height = height
@@ -15,10 +15,13 @@ class Node:
     def __hash__(self):
         return hash(self.__key())
 
+    def __str__(self):
+        return f"id[{self.id}] {self.name}  w/h: ( {self.width} , {self.height} )"
+
     """Because we need to check only the names we don't want
     to check if they are equal based on id. """
-    def __eq__(self, other):
-        if isinstance(other, Node):
+    def __eq__(self, other: "Node"):
+        if self.__class__ == other.__class__:
             return self.name == other.name
             # return self.__key() == other.__key()
         else:
--- project_parser.py
+++ project_parser.py
@@ -1,77 +1,28 @@
-from net import *
-from node import *
-from design import *
-import re
+from classes.node import Node
 
-path_to_nets = r"C:\Users\lenovo\Desktop\Python_Project\Python-Uni-Project\data\design.nets"
-path_to_nodes = r"C:\Users\lenovo\Desktop\Python_Project\Python-Uni-Project\data\design.nodes"
-path_to_pl = r"C:\Users\lenovo\Desktop\Python_Project\Python-Uni-Project\data\design.pl"
-path_to_scl = r"C:\Users\lenovo\Desktop\Python_Project\Python-Uni-Project\data\design.scl"
 
-def get_node_stats():
+class Parser:
+    def __init__(
+        self,
+        path_to_nets: str = "./data/design.nets",
+        path_to_nodes: str = "./data/design.nodes",
+        path_to_pl: str = "./data/design.pl",
+        path_to_scl: str = "./data/design.scl",
+    ):
+        self.path_to_nets = path_to_nets
+        self.path_to_nodes = path_to_nodes
+        self.path_to_pl = path_to_pl
+        self.path_to_scl = path_to_scl
 
-    # Make list here with size of len(nodes)
-    nodes_list = []
-    with open(path_to_nodes, 'r') as nodes_file:
-        nodes = nodes_file.readlines()[7:]
-        nodes_counter = 0
+    def parse_nodes(self):
+        # Make list here with size of len(nodes)
+        nodes_list: list[Node] = []
+        with open(self.path_to_nodes, "r") as nodes_file:
+            nodes_lines = nodes_file.readlines()[7:]
 
-        for line in nodes:
-            if re.match(re.compile(r"\s*[a|p]{1}[0-9]{1,2}\s*"), line):
-
-                nodes_counter += 1
-                i = 0
-                while i < len(line):
-
-                    # Getting the names of the nodes
-                    if re.match(re.compile(r"[a|p]"), line[i-2]) and re.match(re.compile(r"[0-9]"), line[i-1]) and re.match(re.compile(r"[0-9]|\s"), line[i]):
-                        if re.match(re.compile(r"[0-9]"), line[i]):
-                            #print(line[i-2] + line[i-1] + line[i])
-                            name = line[i-2] + line[i-1] + line[i]
-                            #print(name)
-
-                        elif re.match(re.compile(r"\s"), line[i]):
-                            #print(line[i-2] + line[i-1])
-                            name = line[i-2] + line[i-1]
-                            #print(name)
-
-                        # Getting the width of the nodes
-                        if re.match(re.compile(r"\s"), line[i+11]):
-                            #print(line[i+12])
-                            width = line[i+12]
-                            #print(width)
-                        elif re.match(re.compile(r"\s"), line[i+12]):
-                            if re.match(re.compile(r"\s"), line[i+10]):
-                                #print(line[i + 11])
-                                width = line[i + 11]
-                                #print(width)
-                            else:
-                                #print(line[i + 10] + line[i+11])
-                                width = line[i + 10] + line[i+11]
-                                #print(width)
-                        else:
-                            #print(line[i+11] + line[i+12])
-                            width = line[i+11] + line[i+12]
-                            #print(width)
-
-                        # Getting the height of the nodes
-                        if re.match(re.compile(r"\s"), line[i + 23]):
-                            if re.match(re.compile(r"\s"), line[i + 21]):
-                                #print(line[i+22])
-                                height = line[i+22]
-                                #print(height)
-                            else:
-                                #print(line[i+21] + line[i+22])
-                                height = line[i+21] + line[i+22]
-                                #print(height)
-                        else:
-                            #print(line[i+22] + line[i+23])
-                            height = line[i+22] + line[i+23]
-                            #print(height)
-                    i += 1
-
-                # append to list here
-                nodes_list.append( Node(nodes_counter, name, width, height) )
-
-        # Test prints of the nodes_list down here:
-        print(len(nodes_list))
+            for i, line in enumerate(nodes_lines):
+                line_splited = line.split()
+                nodes_list.append(
+                    Node(i, line_splited[0], int(line_splited[1]), int(line_splited[2]))
+                )
+        return nodes_list
